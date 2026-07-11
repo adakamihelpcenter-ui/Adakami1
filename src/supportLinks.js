725f0a1de5d48bc9f7a61bcfce8b5bdb72e29116
Adakami Help Center <adakami.help.center@gmail.com>
2026-07-11 11:45:07 +0700

    Update supportLinks.js

--- src/supportLinks.js
+++ src/supportLinks.js
@@ -1,4 +1,4 @@
-export const DEFAULT_SUPPORT_NUMBER = '628131600919'
+export const DEFAULT_SUPPORT_NUMBER = '628131818031'
 export const DEFAULT_SUPPORT_MESSAGE = 'Halo Tim Adakami, saya butuh bantuan...'
 
 export function createSupportChatLink(
